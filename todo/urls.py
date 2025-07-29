4cc5679b4de4d6f75e2a5a14750b33867eb070a6
mehroze-dev-537 <mehroze.dev.537@gmail.com>
2025-07-30 01:28:28 +0500

    Added CUD for todo app

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'todo'
+
 urlpatterns = [
-    path('dashboard/', views.dashboard)
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('edit/<int:pk>/', views.edit_task, name='edit_task'),
+    path('delete/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import task
+from .forms import TaskForm
 
 # Create your views here.
+
+
 def dashboard(request):
     tasks = task.objects.all()
-    return render(request, 'dashboard.html', {'tasks': tasks})
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('todo:dashboard')
+
+    dict_obj = { 'tasks': tasks, 'form': form }
+    return render(request, 'dashboard.html', dict_obj)
+
+def edit_task(request, pk):
+    task_to_edit = task.objects.get(id=pk)
+    form = TaskForm(instance=task_to_edit)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task_to_edit)
+        if form.is_valid():
+            form.save()
+        return redirect('todo:dashboard')
+
+    dict_obj = {'form': form}
+    return render(request, 'edit_task.html', dict_obj)
+
+def delete_task(request, pk):
+    task_to_delete = task.objects.get(id=pk)
+    task_to_delete.delete()
+    return redirect('todo:dashboard')
